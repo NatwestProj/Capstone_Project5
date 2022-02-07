d75a9d26dced33178d4facb8a4f6078f80b14150
NatwestProj <96770848+NatwestProj@users.noreply.github.com>
2022-02-08 00:12:21 +0530

    lint fail

--- app/app.py
+++ app/app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def print_env_build_number():
-    version = os.environ.get("BUILD_VERSION", default="DEV")
+    version = os.environ.get("BUILD_VERSION", default="DEV");;
     return f"Hello, World! <br> <b>Build version = <i>{version}</i></b>"
